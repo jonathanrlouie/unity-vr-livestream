5f82be7b524f92666b85b87d7fac3f7b23bcf28f
sally pang <sallyxpang_@live.com>
2017-03-09 18:06:14 -0800

    successful network voicechat move to Server

---
+++ Server/Assets/StartScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Types;
+
+public class StartScript : MonoBehaviour {
+	
+	public NetworkManager mng;
+	// Use this for initialization
+	void Start () {
+
+		mng.StartHost();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
