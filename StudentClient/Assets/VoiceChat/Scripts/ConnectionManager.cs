7cafa85a60ca18bc6196d866b5e3f8467ff320c5
sally pang <sallyxpang_@live.com>
2017-03-13 22:04:24 -0700

    scene 1 and 2

---
+++ StudentClient/Assets/VoiceChat/Scripts/ConnectionManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ConnectionManager : MonoBehaviour {
+
+	public void LoadScene(string name) {
+		SceneManager.LoadScene (name);
+	}
+}
